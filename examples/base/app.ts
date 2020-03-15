a2858488861eca554e9dfce5667a892c03d8a42c
mopecat <liuyangdiv@163.com>
2020-03-15 17:40:08 +0800

    feat: request body handle && demo test

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,68 +1,85 @@
 import axios from '../../src/index'
 
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   // 参数为数组
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   // 参数为对象
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
+// const date = new Date()
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   // 参数为日期
+//   params: {
+//     date
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   // 特殊字符支持
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
+
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   // 空值忽略
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
+
+// // 丢弃url中的hash标记
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
+
+// // 保留 url 中已存在的参数
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
+
+// 验证data的demo
 axios({
-  method: 'get',
-  url: '/base/get',
-  // 参数为数组
-  params: {
-    foo: ['bar', 'baz']
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
   }
 })
 
+const arr = new Int32Array([21, 31])
 axios({
-  method: 'get',
-  url: '/base/get',
-  // 参数为对象
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
-const date = new Date()
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  // 参数为日期
-  params: {
-    date
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  // 特殊字符支持
-  params: {
-    foo: '@:$, '
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  // 空值忽略
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
-
-// 丢弃url中的hash标记
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
-
-// 保留 url 中已存在的参数
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -10,10 +11,15 @@ function axios(config: AxiosRequestConfig) {
 // 处理参数
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 // 转换url
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+// 转换data
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 export default axios
